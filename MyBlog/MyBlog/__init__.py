a42893c8a5f16378dcca37417e87b68b3c460a3d
Zeng Yilong <fjzyl@outlook.com>
2016-09-09 10:13:44 +0800

    修改结构，基本抄书

--- MyBlog/MyBlog/__init__.py
+++ MyBlog/MyBlog/__init__.py
@@ -1,15 +1,30 @@
-# -*- coding:utf-8 -*-
-
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+import os
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
 
 
 def create_app(config_name):
     app = Flask(__name__)
-    moment = Moment(app)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
--- MyBlog/runserver.py
+++ MyBlog/runserver.py
@@ -5,16 +5,13 @@ This script runs the MyBlog application using a development server.
 
 
 from os import environ
-from flask_bootstrap import Bootstrap
-from MyBlog import create_app
+from MyBlog import app
 
-app = create_app('no')
 
-if __name__ == '__main__':
-    bootstrap = Bootstrap(app)
+if __name__ == '__main__':    
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT,debug=True)
+    app.run(HOST, PORT)
